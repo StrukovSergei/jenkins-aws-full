e1146f4ded3deb5ceeafc57c2b89620678034f1d
Sergei Strukov <strukov.sergei.999@gmail.com>
2024-12-30 08:52:26 +0200

    Update variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "il-central-1"
+}
+
+variable "cluster_name" {
+  default = "EKS-Cluster"
+}
+
+variable "vpc_cidr" {
+  default = "192.168.0.0/16"
+}
+
+variable "public_subnets" {
+  default = ["192.168.1.0/24", "192.168.2.0/24"]
+}
+
+variable "private_subnets" {
+  default = ["192.168.3.0/24", "192.168.4.0/24"]
+}
